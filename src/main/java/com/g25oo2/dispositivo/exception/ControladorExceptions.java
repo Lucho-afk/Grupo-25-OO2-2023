a5b55e8e9cd8d06c11db4b84cdb6ca79bc5e72dd
Luciano-Leone <lucianoleone2011@hotmail.es>
2023-06-22 20:35:22 -0300

    Proyecto dispositivo (v.019): Manejo de excepciones v.01

---
+++ src/main/java/com/g25oo2/dispositivo/exception/ControladorExceptions.java
@@ -0,0 +1,29 @@
+package com.g25oo2.dispositivo.exception;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class ControladorExceptions {
+
+	public static String exceptions(Exception e) {
+		e.getMessage();
+		return null;
+	}
+
+	public String controlada(String mensaje) {
+		return mensaje;
+	}
+	
+	@GetMapping("/error")
+	public ModelAndView traerDispositivos(Model model) {
+		return new ModelAndView("redirect:/error");
+	}
+
+	@GetMapping("/error/volver")
+	public ModelAndView add() {
+		return new ModelAndView("redirect:/");
+	}
+}
